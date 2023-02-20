2219fcf2d4a97791fd328a22ea29aa4b3ee5d71b
MichailZyusko <michail.zyusko@gmail.com>
2023-02-20 11:12:56 +0300

    feat: add `ServeStaticModule`

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'node:path';
 import { AuthModule } from './modules/auth/auth.module';
 import { GoodsModule } from './modules/goods/goods.module';
 import { MailModule } from './modules/mail/mail.module';
@@ -12,6 +14,9 @@ import { UsersModule } from './modules/users/users.module';
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: ['.env.local', '.env.prod'],
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public/img'),
     }),
     MongooseModule.forRoot(process.env?.MONGO_DB_URL),
     MailModule,
