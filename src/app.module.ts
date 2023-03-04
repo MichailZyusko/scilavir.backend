b9ec254dbd6245330dc4aa83233778fcc314e7f0
MichailZyusko <michail.zyusko@gmail.com>
2023-03-04 12:52:17 +0300

    feat: add errors handeling

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER } from '@nestjs/core';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { PATH_TO_STATIC_FOLDER } from './constants';
+import { HttpErrorFilter } from './errors/http-error.filter';
 import { AuthModule } from './modules/auth/auth.module';
 import { GoodsModule } from './modules/goods/goods.module';
 import { MailModule } from './modules/mail/mail.module';
@@ -26,6 +28,11 @@ import { UsersModule } from './modules/users/users.module';
     GoodsModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpErrorFilter,
+    },
+  ],
 })
 export class AppModule { }
---
+++ src/errors/http-error.filter.ts
@@ -0,0 +1,47 @@
+import 'dotenv/config';
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpStatus,
+} from '@nestjs/common';
+
+type ApiError = {
+  httpCode: number;
+  name: string;
+  message: string;
+  timestamp: string;
+  stack: any;
+  path?: string;
+  method?: string;
+};
+
+const generateError = (e: any): ApiError => {
+  const httpCode = e.status || HttpStatus.INTERNAL_SERVER_ERROR;
+
+  return {
+    httpCode,
+    name: e.name,
+    message: e.message,
+    timestamp: new Date().toISOString(),
+    stack: e.stack,
+  };
+};
+
+@Catch()
+export class HttpErrorFilter implements ExceptionFilter {
+  catch(exception: Error, host: ArgumentsHost): any {
+    const ctx = host.switchToHttp();
+    const request = ctx.getRequest();
+    const response = ctx.getResponse();
+
+    const errorResponse = generateError(exception);
+
+    console.error(
+      `request method: ${request.method} \nrequest url${request.url}\nexception:`,
+      exception,
+    );
+
+    response.status(errorResponse.httpCode).json(errorResponse);
+  }
+}
