42275aeddf50adb3ec7c4a7404546f55bf096626
MichailZyusko <michail.zyusko@gmail.com>
2023-03-04 13:06:07 +0300

    test: add `ping/pong` endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { APP_FILTER } from '@nestjs/core';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { AppController } from './app.controller';
 import { PATH_TO_STATIC_FOLDER } from './constants';
 import { HttpErrorFilter } from './errors/http-error.filter';
 import { AuthModule } from './modules/auth/auth.module';
@@ -27,7 +28,7 @@ import { UsersModule } from './modules/users/users.module';
     AuthModule,
     GoodsModule,
   ],
-  controllers: [],
+  controllers: [AppController],
   providers: [
     {
       provide: APP_FILTER,
