ed26a6bb26e6fcfc2bc2b1b8a1bd6231ba9e088e
MichailZyusko <michail.zyusko@gmail.com>
2023-03-05 15:42:40 +0300

    update cors.ts

--- src/constants/cors.ts
+++ src/constants/cors.ts
@@ -1,7 +1,7 @@
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 export const corsOptions: CorsOptions = {
-  origin: 'https://scilavir-frontend.vercel.app/',
+  origin: 'https://scilavir-frontend.vercel.app',
   methods: ['GET', 'OPTIONS', 'PATCH', 'DELETE', 'POST', 'PUT'],
   allowedHeaders: ['X-CSRF-Token', 'X-Requested-With', 'Accept', 'Accept-Version', 'Content-Length', 'Content-MD5', 'Content-Type', 'Date', 'X-Api-Version'],
   credentials: true,
