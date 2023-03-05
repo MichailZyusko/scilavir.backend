a681b92e8d99fe4f60f28e1b358df015380c6800
MichailZyusko <michail.zyusko@gmail.com>
2023-03-05 13:48:14 +0300

    build(deps): update package-lock.json, package.json and main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   // TODO: Set up `cors` properly
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: ['https://scilavir-frontend.vercel.app', '*'],
-      allowedHeaders: '*',
-      credentials: false,
-      methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
-      preflightContinue: false,
-      optionsSuccessStatus: 204,
-    },
-  });
+  const app = await NestFactory.create(AppModule);
+
+  app.use(cors());
   app.use(cookieParser());
 
   await app.listen(3001);
