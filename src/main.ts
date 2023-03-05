e4fa22be23ef8173d81f94c86b2e5c77e5652bc4
MichailZyusko <michail.zyusko@gmail.com>
2023-03-05 12:21:26 +0300

    fix: update CORS object

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   // TODO: Set up `cors` properly
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: '*',
+      methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+    },
+  });
   app.use(cookieParser());
 
   await app.listen(3001);
