2cb9d2249b15364ff536b202538d1a8c47dfd45c
MichailZyusko <michail.zyusko@gmail.com>
2023-03-05 12:16:35 +0300

    chore: add `TODO` cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // TODO: Set up `cors` properly
   const app = await NestFactory.create(AppModule, { cors: true });
   app.use(cookieParser());
 
