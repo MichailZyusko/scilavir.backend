d871d9d5dc47363c4f08c4d74a820901438b9a50
MichailZyusko <michail.zyusko@gmail.com>
2023-03-05 15:16:46 +0300

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,31 +5,31 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
-const allowCors = (fn) => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-  );
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
-  }
+// const allowCors = (fn) => async (req, res) => {
+//   // res.setHeader('Access-Control-Allow-Credentials', true);
+//   // res.setHeader('Access-Control-Allow-Origin', '*');
+//   // another common pattern
+//   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+//   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+//   res.setHeader(
+//     'Access-Control-Allow-Headers',
+//     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+//   );
+//   if (req.method === 'OPTIONS') {
+//     res.status(200).end();
+//     return;
+//   }
 
-  return await fn(req, res);
-};
+//   return await fn(req, res);
+// };
 
 async function bootstrap() {
   // TODO: Set up `cors` properly
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: {
-      origin: ['https://scilavir-frontend.vercel.app'],
-      methods: ['POST'],
-      allowedHeaders: ['Content-Type', 'Authorization'],
+      origin: '*',
+      methods: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+      allowedHeaders: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
       credentials: true,
       optionsSuccessStatus: 200,
     },
@@ -41,5 +41,3 @@ async function bootstrap() {
   await app.listen(3001);
 }
 bootstrap();
-
-export default allowCors(bootstrap);
