4ae960accb86ccf2134dbc5627e5e1b841abddef
MichailZyusko <michail.zyusko@gmail.com>
2023-03-05 15:32:16 +0300

    build(deps): update package-lock.json, package.json, main.ts and vercel.json

---
+++ src/constants/cors.ts
@@ -0,0 +1,9 @@
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+
+export const corsOptions: CorsOptions = {
+  origin: 'https://scilavir-frontend.vercel.app/',
+  methods: ['GET', 'OPTIONS', 'PATCH', 'DELETE', 'POST', 'PUT'],
+  allowedHeaders: ['X-CSRF-Token', 'X-Requested-With', 'Accept', 'Accept-Version', 'Content-Length', 'Content-MD5', 'Content-Type', 'Date', 'X-Api-Version'],
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,15 @@
-/* eslint-disable @typescript-eslint/return-await */
-/* eslint-disable consistent-return */
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
-
-// const allowCors = (fn) => async (req, res) => {
-//   // res.setHeader('Access-Control-Allow-Credentials', true);
-//   // res.setHeader('Access-Control-Allow-Origin', '*');
-//   // another common pattern
-//   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-//   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-//   res.setHeader(
-//     'Access-Control-Allow-Headers',
-//     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-//   );
-//   if (req.method === 'OPTIONS') {
-//     res.status(200).end();
-//     return;
-//   }
-
-//   return await fn(req, res);
-// };
+import { corsOptions } from './constants/cors';
 
 async function bootstrap() {
-  // TODO: Set up `cors` properly
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    cors: {
-      origin: '*',
-      methods: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-      allowedHeaders: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-      credentials: true,
-      optionsSuccessStatus: 200,
-    },
+    cors: corsOptions,
   });
 
-  // app.setGlobalPrefix('/api');
+  app.setGlobalPrefix('/api');
   app.use(cookieParser());
 
   await app.listen(3001);
