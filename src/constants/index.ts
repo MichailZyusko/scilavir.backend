a839fea2e61e109aa84f904681d75854578d59ff
MichailZyusko <michail.zyusko@gmail.com>
2023-03-05 16:15:19 +0300

    update src/constants/index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,4 @@
+import { join, resolve } from 'node:path';
 import os from 'node:os';
 
-export const PATH_TO_STATIC_FOLDER = os.tmpdir();
+export const PATH_TO_STATIC_FOLDER = join(resolve(), os.tmpdir());
