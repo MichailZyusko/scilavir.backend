3f88b415d533f66afd9db5f1cfab512ec7d850dd
MichailZyusko <michail.zyusko@gmail.com>
2023-03-05 13:03:10 +0300

    feat: update CORS

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   // TODO: Set up `cors` properly
-  const app = await NestFactory.create(AppModule);
-  app.use(cookieParser());
-  app.enableCors({
-    allowedHeaders: '*',
-    origin: '*',
-    credentials: true,
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: ['https://scilavir-frontend.vercel.app', '*'],
+      allowedHeaders: '*',
+      credentials: false,
+      methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+    },
   });
+  app.use(cookieParser());
 
   await app.listen(3001);
 }
