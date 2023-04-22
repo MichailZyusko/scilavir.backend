c6bfc2864ebe6f8b6198ce6cb206ad08ba92d71a
MichailZyusko <michail.zyusko@gmail.com>
2023-04-22 17:11:10 +0300

    update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { WinstonModule } from 'nest-winston';
 import { AppController } from './app.controller';
 import { HttpErrorFilter } from './errors/http-error.filter';
 import { AuthModule } from './modules/auth/auth.module';
-import { ProductsModule } from './modules/products/Products.module';
+import { ProductsModule } from './modules/products/products.module';
 import { MailModule } from './modules/mail/mail.module';
 import { OrdersModule } from './modules/orders/orders.module';
 import { UsersModule } from './modules/users/users.module';
