bf8924dcb257fa82af15e1dde3eac4efa754aa1a
MichailZyusko <michail.zyusko@gmail.com>
2023-04-22 15:06:10 +0300

    delete 1 file and update 1 file

--- src/decorators/user.decorator.ts
+++ src/decorators/user.decorator.ts
@@ -1,10 +1,12 @@
-import { createParamDecorator, type ExecutionContext } from '@nestjs/common';
+import { BadRequestException, createParamDecorator, type ExecutionContext } from '@nestjs/common';
 import { User } from '@supabase/supabase-js';
 
 export const CurrentUser = createParamDecorator(
   (data: unknown, context: ExecutionContext): User => {
     const req: Request & { user: any } = context.switchToHttp().getRequest();
-    const user = req?.user || {};
+    const user = req?.user;
+
+    if (!user) throw new BadRequestException('User not found');
 
     return user;
   },
