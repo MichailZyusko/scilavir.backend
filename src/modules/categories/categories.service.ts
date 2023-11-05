c61e09a460c969634c8434fba550baf7c7187980
Michail Zyusko <67417124+MichailZyusko@users.noreply.github.com>
2023-11-05 12:50:00 +0300

    Dev (#7)
    * update app.module.ts
    
    * update 5 files and delete 1 file
    
    * update mail.service.ts, orders.controller.ts, products.controller.ts and users.controller.ts
    
    * build(deps): update package-lock.json, package.json and supabase-auth.strategy.ts
    
    * feat: add groups & categories
    
    * update 6 files
    
    * update 5 files
    
    * build(deps): update package-lock.json and package.json
    
    * feat: add `cart` & `orders`
    
    * feat: add like/unlike
    
    * feat: add reset-password functionality WIP
    
    * feat: add min price
    
    * feat: add sorting
    
    * feat: add sorting for rest pages
    
    * build: update deps
    
    * feat: integrate `clerk` as main auth service
    
    * feat: fix `env` issues
    
    * feat: add typeorm
    
    * fix(`products`):  improve service
    
    * feat: add `minPrice` for all categories & groups
    
    * feat: add `cart` service
    
    * fix: update db schema
    
    * fix: build issue
    
    * feat: update imports strategy
    
    * fix: use JS code instead of TS
    
    * revert last commit
    
    * chore: prepare to update for `render`
    
    * build(deps): update package-lock.json and package.json
    
    * fix: add `isFavorite` to selection for product
    
    * build(deps): update package-lock.json, package.json, main.ts and products.controller.ts
    
    * feat: add similar products
    
    * feat: add order history
    
    * feat: add ability to `search` products
    
    * feat: add feedbacks functionality
    
    * chore(`feedbacks`): show most recent feedbacks first
    
    * build(deps): update package-lock.json, package.json
    
    * fix(`categories`): update displaying params
    
    * build(deps): update package-lock.json, package.json, categories.controller.ts and products.service.ts

--- src/modules/categories/categories.service.ts
+++ src/modules/categories/categories.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { Repository } from 'typeorm';
+import {
+  FindOptionsSelect, IsNull, Not, Repository,
+} from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Product } from '@products/entity/product.entity';
+import groupBy from 'lodash.groupby';
 import { Category } from './entity/category.entity';
 
 @Injectable()
@@ -14,39 +17,70 @@ export class CategoriesService {
   ) { }
 
   async find() {
-    // TODO add min price to each category
-
-    const [categories, products] = await Promise.all([
-      this.categoriesRepository.find({
-        // TODO check if this works
-        where: { parentId: null },
-        select: ['id', 'name', 'parentId'],
-      }),
-      this.productsRepository.find({
-        select: ['categoryIds', 'price'],
-      }),
-    ]);
-
-    return categories.map(({ id, ...category }) => ({
-      ...category,
-      id,
-      minPrice: products.reduce((acc, product) => {
-        if (product.categoryIds.includes(id)) {
-          return acc < product.price ? acc : product.price;
-        }
-
-        return acc;
-      }, Infinity),
-    }));
-  }
-
-  async findSubCategories(id: string) {
     return this.categoriesRepository.find({
-      where: { parentId: id },
+      where: { parentId: Not(IsNull()) },
       select: ['id', 'name', 'parentId'],
     });
   }
 
+  async findRootCategoriesWithSmallestPrice() {
+    // TODO add min price to each category via SQL
+    const categories = await this.categoriesRepository.find({
+      select: ['id', 'name', 'parentId'],
+    });
+    const nonRootCategories = categories.filter((item) => item.parentId);
+    const rootCategories = categories.filter((item) => !item.parentId);
+
+    const products = await this.productsRepository.createQueryBuilder()
+      .select('p')
+      .from(Product, 'p')
+      .where('p.categoryIds && :categoryIds', {
+        categoryIds: nonRootCategories.map(({ id }) => id),
+      })
+      .getMany();
+
+    const gropedCategories = groupBy(nonRootCategories, 'parentId');
+
+    return rootCategories.map(({ id, ...category }) => {
+      const minPriceByCategory = gropedCategories[id]
+        .map(({ id: categoryId }) => products.reduce((acc, product) => {
+          if (product.categoryIds.includes(categoryId)) {
+            return acc < product.price ? acc : product.price;
+          }
+
+          return acc;
+        }, Infinity));
+
+      return {
+        ...category,
+        id,
+        minPrice: Math.min(...minPriceByCategory),
+      };
+    });
+  }
+
+  async findCategory(id: string) {
+    const select = ['id', 'name'] as FindOptionsSelect<Category>;
+    const [category, subCategories] = await Promise.all([
+      this.categoriesRepository.findOne({
+        where: { id },
+        select,
+      }),
+      this.categoriesRepository.find({
+        where: { parentId: id },
+        select,
+      }),
+    ]);
+
+    return {
+      category: {
+        ...category,
+        subCategories,
+      },
+    };
+  }
+
+  // TODO: exclude same products as for we are looking for
   async findSimilarProductsByCategoryId(id: string) {
     return this.productsRepository.createQueryBuilder()
       .select('p')
