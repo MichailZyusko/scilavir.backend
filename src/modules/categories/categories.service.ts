89680c5b7b2dda1330e507989e5b1689edda0517
MichailZyusko <michail.zyusko@gmail.com>
2023-12-12 22:57:24 +0300

    fix: remove duplicated code

--- src/modules/categories/categories.service.ts
+++ src/modules/categories/categories.service.ts
@@ -2,9 +2,6 @@ import { Injectable } from '@nestjs/common';
 import {
   FindOptionsSelect, IsNull, Not, Repository,
 } from 'typeorm';
-import {
-  FindOptionsSelect, IsNull, Not, Repository,
-} from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Product } from '@products/entity/product.entity';
 import groupBy from 'lodash.groupby';
@@ -53,7 +50,6 @@ export class CategoriesService {
 
   async find() {
     return this.categoriesRepository.find({
-      where: { parentId: Not(IsNull()) },
       where: { parentId: Not(IsNull()) },
       select: ['id', 'name', 'parentId'],
     });
